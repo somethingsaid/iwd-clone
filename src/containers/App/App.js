b55e86f3c972e497e93731805e21cc0595731507
somethingsaid <kwong.billy@gmail.com>
2020-09-22 11:36:28 -0400

    Trying CI again with github_token

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is a new header</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
