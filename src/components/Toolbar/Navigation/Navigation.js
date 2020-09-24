b64906d6203ca03956358b857d8cb4eacef2f66c
somethingsaid <kwong.billy@gmail.com>
2020-09-24 11:02:04 -0400

    Updated nav items object definition

--- src/components/Toolbar/Navigation/Navigation.js
+++ src/components/Toolbar/Navigation/Navigation.js
@@ -4,16 +4,42 @@ import "./Navigation.css";
 
 const navigation = () => {
   const items = [
-    "Software Development",
-    "Why Intelliware",
-    "Resources",
-    "Careers",
+    {
+      main: "Software Development",
+      secondary: [
+        "Our Services",
+        "Focus Areas",
+        "The Intelliware Approach",
+        "Success Stories",
+      ],
+    },
+    {
+      main: "Why Intelliware",
+      secondary: [
+        "Our Agile Journey",
+        "Technology Leadership",
+        "Community Initiatives",
+        "Leadership Team",
+      ],
+    },
+    {
+      main: "Resources",
+      secondary: [
+        "News, Updates and Insights",
+        "Agile Resources",
+        "Our Research",
+      ],
+    },
+    {
+      main: "Careers",
+      secondary: ["Why Work Here", "Current Opportunities", "Applicant FAQs"],
+    },
   ];
 
   return (
     <ul className="Navigation">
       {items.reverse().map((item) => {
-        return <NavItem key={item} description={item} />;
+        return <NavItem key={item.main} description={item.main} />;
       })}
     </ul>
   );
