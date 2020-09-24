bb22a6af052b4cd61bc2f39c83051747a9a35024
somethingsaid <kwong.billy@gmail.com>
2020-09-24 10:08:47 -0400

    Add hover effect to button

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "./Logo/Logo";
 import Navigation from "./Navigation/Navigation";
 import ActionButton from "../ActionButton/ActionButton";
 import "./Toolbar.css";
 
-const toolbar = () => {
-  return (
-    <div className="Toolbar">
-      <div className="Toolbar-logo">
-        <Logo />
-      </div>
+class Toolbar extends Component {
+  state = {
+    showActions: true,
+  };
+
+  handleWindowResize = () => {
+    this.setState({
+      showActions: window.innerWidth >= 768,
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize);
+  }
+
+  render() {
+    let navigation = (
       <div className="Toolbar-navigation">
         <Navigation />
       </div>
+    );
+    let actionBtn = (
       <div className="Toolbar-action-button">
         <ActionButton btnText="CONTACT US" />
       </div>
-    </div>
-  );
-};
+    );
 
-export default toolbar;
+    if (!this.state.showActions) {
+      navigation = null;
+      actionBtn = null;
+    }
+    return (
+      <div className="Toolbar">
+        <div className="Toolbar-logo">
+          <Logo />
+        </div>
+        {navigation}
+        {actionBtn}
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
