1b2f6f7415d9d803f3ab99603936e2cf3a264884
somethingsaid <kwong.billy@gmail.com>
2020-09-24 12:42:41 -0400

    Center text on video

--- src/components/Backsplash/Backsplash.js
+++ src/components/Backsplash/Backsplash.js
@@ -4,7 +4,20 @@ import "./Backsplash.css";
 const backsplash = () => {
   const src =
     "https://www.intelliware.com/wp-content/uploads/2019/11/Website20Video20Stabalized-21.mp4";
-  return <video src={src} className="Backsplash" muted loop autoPlay></video>;
+  return (
+    <div className="Backsplash">
+      <video src={src} className="Video" muted loop autoPlay></video>
+      <div className="overlay">
+        <h1>Intelliware develops enterprise-grade software solutions</h1>
+        <h3>
+          We build software that enables organizations in complex industries to
+          achieve their business objectives. Invest in enterprise-grade software
+          solutions that deliver substantive business value and enduring
+          competitive advantage.
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default backsplash;
