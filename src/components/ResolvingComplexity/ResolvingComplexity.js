065280cc8d25185e7e5c4b2b433b30cd84302405
somethingsaid <kwong.billy@gmail.com>
2020-09-30 14:46:02 -0400

    Give credit to image background

--- src/components/ResolvingComplexity/ResolvingComplexity.js
+++ src/components/ResolvingComplexity/ResolvingComplexity.js
@@ -15,6 +15,16 @@ const resolvingComplexity = () => {
         <LearnMore url="https://www.intelliware.com/focus-areas/" />
       </div>
       <div className={classes.placeholder}></div>
+      <span className={classes.imageCredit}>
+        Photo by{" "}
+        <a href="https://unsplash.com/@hishahadat?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+          Shahadat Rahman
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/s/photos/complexity?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </span>
     </div>
   );
 };
