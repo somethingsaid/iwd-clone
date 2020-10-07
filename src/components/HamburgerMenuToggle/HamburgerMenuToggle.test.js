21005886e7fd6420fec5a07a5a68655db7b9c256
somethingsaid <kwong.billy@gmail.com>
2020-10-07 11:07:41 -0400

    Added HamburgerMenuToggle.test.js

---
+++ src/components/HamburgerMenuToggle/HamburgerMenuToggle.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import HamburgerMenuToggle from "./HamburgerMenuToggle";
+
+configure({ adapter: new Adapter() });
+
+describe("<HamburgerMenuToggle />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<HamburgerMenuToggle />);
+  });
+
+  it("should render a font-awesome icon with fa-bars class", () => {
+    expect(wrapper.find("i.fa-bars")).toHaveLength(1);
+  });
+});
