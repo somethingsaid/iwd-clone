a986094045c6cf6a7bbfa591de6ba17c383c4fb3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-07 18:42:42 +0000

    Deploy somethingsaid/iwd-clone to github.com/somethingsaid/iwd-clone.git:gh-pages

---
+++ precache-manifest.9b730af606038b00330078e9ce221c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02304b158072602372ad8142086335e5",
+    "url": "/iwd-clone/index.html"
+  },
+  {
+    "revision": "8ad345a55f6471d05e37",
+    "url": "/iwd-clone/static/css/main.7396532c.chunk.css"
+  },
+  {
+    "revision": "8a77f44ac36fe4cc4da2",
+    "url": "/iwd-clone/static/js/2.02d330ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iwd-clone/static/js/2.02d330ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad345a55f6471d05e37",
+    "url": "/iwd-clone/static/js/main.0b9a17e2.chunk.js"
+  },
+  {
+    "revision": "7cd757c7e39c793757a3",
+    "url": "/iwd-clone/static/js/runtime-main.1f24534b.js"
+  }
+]);
